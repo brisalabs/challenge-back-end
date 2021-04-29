2faeb72080ecb551af6964fb84af0f57f8ac04f7
mpgxc <mpgx5.c@gmail.com>
2021-04-29 14:45:07 -0300

    feat: Add minimal http server setup

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,6 +1,16 @@
+import 'express-async-errors';
 import 'reflect-metadata';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import { router } from './routes';
+import '../database';
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(helmet());
 
 export { app };
