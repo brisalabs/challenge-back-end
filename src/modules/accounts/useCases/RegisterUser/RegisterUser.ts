d6c9ae668af0bfdd0ce4677513be62413f9887f5
mpgxc <mpgx5.c@gmail.com>
2021-04-30 16:45:32 -0300

    feat: Added RegisterUser useCase with test

---
+++ src/modules/accounts/useCases/RegisterUser/RegisterUser.ts
@@ -0,0 +1,52 @@
+import { IUserDTO, User } from '../../domain/user/entity';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+class RegisterUser {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    document_id_cpf,
+    email,
+    name,
+    password,
+    phone_number,
+    username,
+  }: IUserDTO): Promise<User> {
+    const okProps =
+      !document_id_cpf ||
+      !email ||
+      !name ||
+      !password ||
+      !phone_number ||
+      !username;
+
+    if (okProps) {
+      throw new Error('User Missing Props!');
+    }
+
+    const userExists =
+      (await this.usersRepository.findByEmail(email)) ||
+      (await this.usersRepository.findByDocumentId(document_id_cpf));
+
+    if (userExists) {
+      throw new Error('User Already Exists!');
+    }
+
+    const userObject = new User();
+
+    const user = userObject.create({
+      document_id_cpf,
+      email,
+      name,
+      password,
+      phone_number,
+      username,
+    });
+
+    await this.usersRepository.create(user);
+
+    return user;
+  }
+}
+
+export { RegisterUser };
