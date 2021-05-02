cec3eeed2891258943d3ebbc07053e668209485f
mpgxc <mpgx5.c@gmail.com>
2021-05-02 01:28:28 -0300

    chore: Change formt file and added env vars

---
+++ ormconfig.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+export default {
+  type: 'postgres',
+  host: 'localhost',
+  logging: true,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['./src/modules/**/domain/*.ts'],
+  migrations: ['./src/infra/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/infra/database/migrations',
+  },
+};
