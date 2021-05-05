07089b8ab0c7320aaeb4fee733ca9233266f630a
mpgxc <mpgx5.c@gmail.com>
2021-05-05 03:24:59 -0300

    refact(core): Added contructor properties to generate by default

--- src/core/BaseEntity.ts
+++ src/core/BaseEntity.ts
@@ -1,22 +1,37 @@
 import {
-  PrimaryGeneratedColumn,
+  PrimaryColumn,
   UpdateDateColumn,
   CreateDateColumn,
   Column,
 } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 abstract class BaseEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryColumn('uuid')
+  readonly id?: string;
 
   @Column({ default: true })
-  active: boolean;
+  readonly is_active: boolean;
 
   @CreateDateColumn({ type: 'timestamp' })
-  readonly created_at: Date;
+  readonly created_at?: Date;
 
   @UpdateDateColumn({ type: 'timestamp' })
-  readonly updated_at: Date;
+  readonly updated_at?: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+
+    if (!this.updated_at) {
+      this.updated_at = new Date();
+    }
+
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+  }
 }
 
 export { BaseEntity };
